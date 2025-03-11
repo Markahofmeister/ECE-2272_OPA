292c012fe2d03ffec4b45804d8ce80bcef467c1a
markahofmeister <markhof12@gmail.com>
2025-03-11 10:24:04 -0400

    duplicating 1 done.

--- gds/gds_scripts/OPA_layout_subLambda.py
+++ gds/gds_scripts/OPA_layout_subLambda.py
@@ -23,7 +23,7 @@ ysepMax = elementSeparation * (numElements - 1)
 fa_pitch = 127.0                     # um
 
 # Create duplicate designs?
-duplicate = False
+duplicate = True
 
 
 # DERIVED FROM Cornerstone grating coupler "SOI220nm_1550nm_TE_RIB_Grating_Coupler"
@@ -38,153 +38,256 @@ def gc_cornerstone_pdk_subLambda(width_grating: float = 1.55) -> gf.Component:
 xs = cs_pdk.cornerstone_rib()
 
 # Cornerstone Library
-gc = gc_cornerstone_pdk_subLambda(width_grating=1.0)
 fgc = cs_pdk.gc_focusing_cornerstone_pdk()
 mmi1x2 = cs_pdk.mmi1x2_cornerstone_pdk()
 mmi2x2 = cs_pdk.mmi2x2_cornerstone_pdk()
 wgx = cs_pdk.crossing_cornerstone_pdk()
 
 
+if(duplicate):     
+
+     # top cell
+    top = gf.Component('TOP')
+
+    numCopies = 3
+
+    gratingWidths = [1.0, 1.5, 2.0]
+
+    for copy in range(numCopies):
+
+        gc = gc_cornerstone_pdk_subLambda(width_grating=gratingWidths[copy])
+
+        OPATemp = gf.Component("OPA_subL" + str(copy))
+
+        re = gf.Component("Radiating Elements" + str(copy))
+
+        radiatingElements = list()
+
+        for i in range(numElements):
+
+            radiatingElements.insert(i, re << gc)
+            radiatingElements[i].movex( (die_width / 2) - xMargin)
+            radiatingElements[i].movey( (ysepMax / 2) - (elementSeparation * i) )
+
+        # Add radiating elements to top
+        OPATemp << re
 
 
+        # Splitter
+        pdiv = gf.Component('Power Division' + str(copy))
 
+        # Number of required MMI splitter stages
+        numStages = round(np.log2(numElements))
 
+        # List of lists of MMI splitters
+        splitters = []
 
+        # Generate, place, and route N = log2(numElements) MMI splitters 
+        for stage in range( numStages ):
 
+            temp_splitters = []
 
+            divisor = 2**(stage + 1)
 
+            # Generate and place MMI splitters for stage M 
+            for i in range( round( numElements / divisor ) ):
 
+                temp_splitters.insert(i, pdiv << mmi1x2)
+                temp_splitters[i].movex( (die_width / 2) - xMargin - (splitter_Xsep * (stage + 1)) )
+                temp_splitters[i].movey( (ysepMax / 2) - ((divisor-1) * (elementSeparation / 2)) - (elementSeparation * i * divisor) )
 
-# top cell
-top = gf.Component('TOP')
+            # Add stage M of splitters to master MMI splitter list 
+            splitters.append(temp_splitters)
 
-OPA = gf.Component('OPA')
+            # If we are not on the stage that connects to the bragg gratings, 
+            # draw the connections betweem gratings 
+            if(stage != 0):
+                rg = round( (numElements * 2) / divisor)
+                for i in range( rg ):
+                    if(i % 2 == 0):
+                        gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o2'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
+                    else:
+                        gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o3'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
+                    
 
-re = gf.Component('Radiating Elements')
+        OPATemp << pdiv
 
-radiatingElements = list()
-
-for i in range(numElements):
-
-    radiatingElements.insert(i, re << gc)
-    radiatingElements[i].movex( (die_width / 2) - xMargin)
-    radiatingElements[i].movey( (ysepMax / 2) - (elementSeparation * i) )
-
-# Add radiating elements to top
-OPA << re
-
-
-# Splitter
-pdiv = gf.Component('Power Division')
-
-# Number of required MMI splitter stages
-numStages = round(np.log2(numElements))
-
-# List of lists of MMI splitters
-splitters = []
-
-# Generate, place, and route N = log2(numElements) MMI splitters 
-for stage in range( numStages ):
-
-    temp_splitters = []
-
-    divisor = 2**(stage + 1)
-
-    # Generate and place MMI splitters for stage M 
-    for i in range( round( numElements / divisor ) ):
-
-        temp_splitters.insert(i, pdiv << mmi1x2)
-        temp_splitters[i].movex( (die_width / 2) - xMargin - (splitter_Xsep * (stage + 1)) )
-        temp_splitters[i].movey( (ysepMax / 2) - ((divisor-1) * (elementSeparation / 2)) - (elementSeparation * i * divisor) )
-
-    # Add stage M of splitters to master MMI splitter list 
-    splitters.append(temp_splitters)
-
-    # If we are not on the stage that connects to the bragg gratings, 
-    # draw the connections betweem gratings 
-    if(stage != 0):
-        rg = round( (numElements * 2) / divisor)
-        for i in range( rg ):
-            if(i % 2 == 0):
-                gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o2'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
-            else:
-                gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o3'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
+        # Route final splitter stage to radiating elements 
+        for j in range(numElements):
             
-
-OPA << pdiv
-
-# Route final splitter stage to radiating elements 
-for i in range(numElements):
-    if(i % 2 == 0):
-        gf.routing.route_dubin(OPA, port1=splitters[0][int(i/2)].ports['o2'], port2=radiatingElements[i].ports['o1'], cross_section=xs)
-    else:
-        gf.routing.route_dubin(OPA, port1=splitters[0][int(i/2)].ports['o3'], port2=radiatingElements[i].ports['o1'], cross_section=xs)
+            if(j % 2 == 0):
+                gf.routing.route_dubin(OPATemp, port1=splitters[0][int(j/2)].ports['o2'], port2=radiatingElements[j].ports['o1'], cross_section=xs)
+            else:
+                gf.routing.route_dubin(OPATemp, port1=splitters[0][int(j/2)].ports['o3'], port2=radiatingElements[j].ports['o1'], cross_section=xs)
 
 
 
-# Input Grating Coupler
-gIn = gf.Component('gratingIn')
+        # Input Grating Coupler
+        gIn = gf.Component('gratingIn' + str(copy))
 
-# Position Input Grating Coupler
-# Position Input Grating Coupler
-gratingIn = gIn << fgc
-mov =  OPA.xmax  - OPA.xsize - 50
-gratingIn.rotate(180.0)
-gratingIn.movex(mov)
+        # Position Input Grating Coupler
+        # Position Input Grating Coupler
+        gratingIn = gIn << fgc
+        mov =  OPATemp.xmax  - OPATemp.xsize - 50
+        gratingIn.rotate(180.0)
+        gratingIn.movex(mov)
 
-# Add to top and route waveguide
-OPA << gIn
-gf.routing.route_single(OPA, port1=gratingIn.ports['o1'], port2=splitters[numStages - 1][0].ports['o1'], cross_section=xs)
+        # Add to top and route waveguide
+        OPATemp << gIn
+        gf.routing.route_single(OPATemp, port1=gratingIn.ports['o1'], port2=splitters[numStages - 1][0].ports['o1'], cross_section=xs)
 
-# Input Calibration Grating Coupler 
-cIn = gf.Component('Calibrator Input')
-calIn = cIn << fgc
-calIn.movex(gIn.x - (gIn.xsize / 1))
-calIn.movey(gIn.y + fa_pitch)
+        # Input Calibration Grating Coupler 
+        cIn = gf.Component('Calibrator Input' + str(copy))
+        calIn = cIn << fgc
+        calIn.movex(gIn.x - (gIn.xsize / 1))
+        calIn.movey(gIn.y + fa_pitch)
 
-# Output Calibration Grating Coupler 
-cOut = gf.Component('Calibrator Output')
-calOut = cOut << fgc
-calOut.movex(gIn.x - (gIn.xsize / 1))
-calOut.movey(gIn.y - fa_pitch)
+        # Output Calibration Grating Coupler 
+        cOut = gf.Component('Calibrator Output' + str(copy))
+        calOut = cOut << fgc
+        calOut.movex(gIn.x - (gIn.xsize / 1))
+        calOut.movey(gIn.y - fa_pitch)
 
-OPA << cIn
-OPA << cOut
+        OPATemp << cIn
+        OPATemp << cOut
 
-gf.routing.route_single(OPA, port1=calIn.ports['o1'], port2=calOut.ports['o1'], cross_section=xs, radius = 20.0)
+        gf.routing.route_single(OPATemp, port1=calIn.ports['o1'], port2=calOut.ports['o1'], cross_section=xs, radius = 20.0)
 
-# floor plan
-# This is the outline of the available space 
-# ^^ It is on layer 99 because of it.
-top << gf.components.rectangle(size=(die_width, die_height), centered=True, layer=(99,0))
+        OPAIntermediate = gf.Component("Intermediate" + str(copy))
+        OPAIntermediate << OPATemp
 
-# Add OPA to top level cell
-# top << OPA
+        OPAIntermediate.movey(400 * copy)
 
-dup = gf.Component('dup')
+        # Add OPA to top level cell
+        top << OPAIntermediate
 
-if(duplicate):
+        # deviceMargin = 150      # Buffer between device copies, um
 
-    deviceMargin = 150      # Buffer between device copies, um
+        # opaDimX = OPA.xsize
+        # opaDimY = OPA.ysize
 
-    opaDimX = OPA.xsize
-    opaDimY = OPA.ysize
+        # deviceYSizeBuff = opaDimY + deviceMargin
+        # numCopies = m.floor(((die_height - (yMargin * 2)) / deviceYSizeBuff) )
 
-    deviceYSizeBuff = opaDimY + deviceMargin
-    numCopies = m.floor(((die_height - (yMargin * 2)) / deviceYSizeBuff) )
-    # print(numCopies)
 
-    for i in range(numCopies):
-        topTemp = gf.Component("topTemp" + str(i))
-        topTemp << OPA
-        yPos = (die_height / 2) - ((deviceYSizeBuff) * (i + 1))
-        topTemp.movey(yPos)
-        # dup << topTemp
-        top << topTemp
+    # floor plan
+    # This is the outline of the available space 
+    # ^^ It is on layer 99 because of it.
+    top << gf.components.rectangle(size=(die_width, die_height), centered=True, layer=(99,0))
+        
+    top.plot()
+    top.show()
     
 
 else:
+
+    gc = gc_cornerstone_pdk_subLambda(width_grating=1.0)
+
+    # top cell
+    top = gf.Component('TOP')
+
+    OPA = gf.Component('OPA')
+
+    re = gf.Component('Radiating Elements')
+
+    radiatingElements = list()
+
+    for i in range(numElements):
+
+        radiatingElements.insert(i, re << gc)
+        radiatingElements[i].movex( (die_width / 2) - xMargin)
+        radiatingElements[i].movey( (ysepMax / 2) - (elementSeparation * i) )
+
+    # Add radiating elements to top
+    OPA << re
+
+
+    # Splitter
+    pdiv = gf.Component('Power Division')
+
+    # Number of required MMI splitter stages
+    numStages = round(np.log2(numElements))
+
+    # List of lists of MMI splitters
+    splitters = []
+
+    # Generate, place, and route N = log2(numElements) MMI splitters 
+    for stage in range( numStages ):
+
+        temp_splitters = []
+
+        divisor = 2**(stage + 1)
+
+        # Generate and place MMI splitters for stage M 
+        for i in range( round( numElements / divisor ) ):
+
+            temp_splitters.insert(i, pdiv << mmi1x2)
+            temp_splitters[i].movex( (die_width / 2) - xMargin - (splitter_Xsep * (stage + 1)) )
+            temp_splitters[i].movey( (ysepMax / 2) - ((divisor-1) * (elementSeparation / 2)) - (elementSeparation * i * divisor) )
+
+        # Add stage M of splitters to master MMI splitter list 
+        splitters.append(temp_splitters)
+
+        # If we are not on the stage that connects to the bragg gratings, 
+        # draw the connections betweem gratings 
+        if(stage != 0):
+            rg = round( (numElements * 2) / divisor)
+            for i in range( rg ):
+                if(i % 2 == 0):
+                    gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o2'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
+                else:
+                    gf.routing.route_dubin(pdiv, port1=splitters[stage][int(i/2)].ports['o3'], port2=splitters[stage-1][i].ports['o1'], cross_section=xs)
+                
+
+    OPA << pdiv
+
+    # Route final splitter stage to radiating elements 
+    for i in range(numElements):
+        if(i % 2 == 0):
+            gf.routing.route_dubin(OPA, port1=splitters[0][int(i/2)].ports['o2'], port2=radiatingElements[i].ports['o1'], cross_section=xs)
+        else:
+            gf.routing.route_dubin(OPA, port1=splitters[0][int(i/2)].ports['o3'], port2=radiatingElements[i].ports['o1'], cross_section=xs)
+
+
+
+    # Input Grating Coupler
+    gIn = gf.Component('gratingIn')
+
+    # Position Input Grating Coupler
+    # Position Input Grating Coupler
+    gratingIn = gIn << fgc
+    mov =  OPA.xmax  - OPA.xsize - 50
+    gratingIn.rotate(180.0)
+    gratingIn.movex(mov)
+
+    # Add to top and route waveguide
+    OPA << gIn
+    gf.routing.route_single(OPA, port1=gratingIn.ports['o1'], port2=splitters[numStages - 1][0].ports['o1'], cross_section=xs)
+
+    # Input Calibration Grating Coupler 
+    cIn = gf.Component('Calibrator Input')
+    calIn = cIn << fgc
+    calIn.movex(gIn.x - (gIn.xsize / 1))
+    calIn.movey(gIn.y + fa_pitch)
+
+    # Output Calibration Grating Coupler 
+    cOut = gf.Component('Calibrator Output')
+    calOut = cOut << fgc
+    calOut.movex(gIn.x - (gIn.xsize / 1))
+    calOut.movey(gIn.y - fa_pitch)
+
+    OPA << cIn
+    OPA << cOut
+
+    gf.routing.route_single(OPA, port1=calIn.ports['o1'], port2=calOut.ports['o1'], cross_section=xs, radius = 20.0)
+
+    # floor plan
+    # This is the outline of the available space 
+    # ^^ It is on layer 99 because of it.
+    top << gf.components.rectangle(size=(die_width, die_height), centered=True, layer=(99,0))
+
+    # Add OPA to top level cell
     top << OPA
 
-top.plot()
-top.show()
+    top.plot()
+    top.show()
